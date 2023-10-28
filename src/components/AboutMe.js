c41a59b0c71c54ef92233152bc0b733894e514f2
Deepa Jha <deepadj1415@gmail.com>
2023-10-29 02:01:26 +0530

    Update Resume Link

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,7 @@ export default function AboutMe({ id }) {
           I'm <span> Deepa </span>, a final year student pursuing <span>B.Tech in CSE </span> from New Delhi, India. I build websites with a focus on responsiveness, accessibility and pleasing aesthetics. Being well versed in all the key languages allows me to rapidly design, code and deploy in an organized and efficient manner. While I specialize in <span>frontend development,</span> I'm also delving into <span>backend technologies.</span> I'm all ears for <span>new opportunities</span> to create awesome software solutions. Let's team up and cook up some digital wonders together.
           <br/> <br/>
           Beyond coding, <span>I'm an artist at heart❤️</span> – you'll often find me lost in the world of sketches and colors. I really enjoy being creative, so I like to spend my free time drawing, sketching, and painting. It's a great way for me to unwind and tap into my creative side.
-          Want to know more about me? Feel free to <a className='resume' href="https://drive.google.com/file/d/1ERM3kdM2vyQPlxnuo4SCPrFJI7lE5NZi/view?usp=sharing" target="_blank" rel="noopener noreferrer">explore my resume.</a>
+          Want to know more about me? Feel free to <a className='resume' href="https://drive.google.com/file/d/1TmFcDgl3PBl2XAjjtfwdwjQwmhb9_CWz/view?usp=sharing" target="_blank" rel="noopener noreferrer">explore my resume.</a>
         </div>
         <div className="profile-photo">
           <div className="heading"> About Me</div>
